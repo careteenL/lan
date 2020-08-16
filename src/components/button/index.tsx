ded77159c8b8b52c7d77ca9a81df3c90c7ac3b5f
CareteenL <15074806497@163.com>
2020-08-16 18:20:40 +0800

    feat: 🎸 config global css

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,17 @@
 import React, { ButtonHTMLAttributes, PropsWithChildren } from "react";
 
+import styled from 'styled-components';
+
+const LanButton = styled.button({
+	color: 'red',
+})
+
+console.log(styled, styled.button)
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 function Button(props: PropsWithChildren<ButtonProps>) {
 	const { children, ...rest } = props;
-	return <button {...rest}>{children}</button>;
+	return <LanButton {...rest}>{children}</LanButton>;
 }
 export default Button;
