d716d28bbf6ac38e43043ac20c93f1339a147246
CareteenL <15074806497@163.com>
2020-08-23 12:06:36 +0800

    test: 💍 fixed icon test unit

--- src/components/icon/index.test.tsx
+++ src/components/icon/index.test.tsx
@@ -4,9 +4,9 @@ import { Icon, IconProps } from "./index";
 import { icons } from "../shared/icons";
 
 function IconTest(icon: IconProps["icon"]) {
-	const wrapper = render(<Icon icon={icon}></Icon>);
+	const wrapper = render(<Icon data-testid="icon-path" icon={icon}></Icon>);
 	const path = wrapper.queryByTestId("icon-path");
-	expect(path).toHaveAttribute("d", icons[icon]);
+	expect(path.children[0]).toHaveAttribute("d", icons[icon]);
 	cleanup();
 }
 
@@ -17,16 +17,16 @@ describe(" test Icon component", () => {
 		});
 	});
 	it("it should render  block ", () => {
-		const wrapper = render(<Icon icon="mobile" block></Icon>);
+		const wrapper = render(<Icon data-testid="icon-svg" icon="mobile" block></Icon>);
 		const svg = wrapper.getByTestId("icon-svg");
 		expect(svg).toHaveStyle("display:block");
 	});
 	it("it should render correct color ", () => {
-		let wrapper = render(<Icon icon="mobile"></Icon>);
+		let wrapper = render(<Icon data-testid="icon-path" icon="mobile"></Icon>);
 		let path = wrapper.queryByTestId("icon-path");
 		expect(path).toHaveAttribute("color", "black");
 		cleanup();
-		wrapper = render(<Icon icon="mobile" color="blue"></Icon>);
+		wrapper = render(<Icon data-testid="icon-path" icon="mobile" color="blue"></Icon>);
 		path = wrapper.queryByTestId("icon-path");
 		expect(path).toHaveAttribute("color", "blue");
 	});
