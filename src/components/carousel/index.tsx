ff1ecf3ddcae6d6d1b021abe1fe847755d3007db
CareteenL <15074806497@163.com>
2020-08-31 18:58:09 +0800

    docs: ✏️ remove unuse codes

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -10,9 +10,9 @@ import React, {
 } from "react";
 import styled from "styled-components";
 import Radio from "../radio";
-import { color, typography } from "../shared/styles";
-import { darken, rgba, opacify } from "polished";
-import { easing } from "../shared/animation";
+import { color } from "../shared/styles";
+// import { darken, rgba, opacify } from "polished";
+// import { easing } from "../shared/animation";
 
 interface AnimationType {
   animatein: boolean;
