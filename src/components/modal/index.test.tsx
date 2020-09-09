8ba7a2bf6735f0b6bb2c64e7dae78c0d6d35d793
CareteenL <15074806497@163.com>
2020-09-09 14:05:29 +0800

    test: 💍 add modal tests

---
+++ src/components/modal/index.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Modal } from "./index";
+
+describe("test Modal component", () => {
+
+	it("should render text", () => {
+		const wrapper = render(
+			<Modal
+        visible={true}
+        parentSetState={() => {}}
+			></Modal>
+		);
+		expect(wrapper).toMatchSnapshot();
+	});
+});
