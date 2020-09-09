00cc10398c5347f9a6a407d37aa20470734b4f32
CareteenL <15074806497@163.com>
2020-09-09 11:15:28 +0800

    feat: 🎸 complete message

--- src/components/message/index.stories.tsx
+++ src/components/message/index.stories.tsx
@@ -1,21 +1,71 @@
 import React from "react";
-import { Button } from "../..";
-import  {
-	createMessage,
+import { Button, Icon } from "../..";
+import  message, {
+	MessageType,
 } from "./index";
 import {
-	withKnobs,
+	withKnobs, select, number, color, text,
 } from "@storybook/addon-knobs";
 
 export default {
 	title: "Display/Message",
-	component: createMessage,
+	component: message,
 	decorators: [withKnobs],
 };
 
-export const knobsMessage = () => (
+const Options: MessageType[] = [
+	"info",
+	"success",
+	"error",
+	"warning",
+	"loading",
+	"default",
+];
+
+export const knobsMessage = () => {
+	const se = select<MessageType>("iconType", Options, "default");
+	const op = {
+		delay: number("delay", 2000),
+		animationDuring: number("animationDuring", 300),
+		background: color("background", "#fff"),
+		color: color("color", "#333"),
+	};
+	const tx = text("content", "hello message");
+	const onClick = () => message[se](tx, op);
+
+	return (
+		<div>
+			<Button onClick={onClick}>click</Button>
+		</div>
+	);
+};
+
+export const callbackTest = () => (
 	<div>
-		<Button onClick={() => createMessage()("Careteen")}>click</Button>
-		<Button onClick={() => createMessage()("Lan")}>click</Button>
+		<Button
+			onClick={() =>
+				message.loading("加载中", {
+					callback: () => message.success("加载完成"),
+				})
+			}
+		>
+			callback
+		</Button>
+	</div>
+);
+
+export const withIcon = () => (
+	<div>
+		<Button
+			onClick={() =>
+				message.default(
+					<span>
+						<Icon icon="admin"></Icon>111
+					</span>
+				)
+			}
+		>
+			callback
+		</Button>
 	</div>
 );
