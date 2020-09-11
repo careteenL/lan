442e3995f14c9e6bee2dac4361a6f3d916e58e0f
CareteenL <15074806497@163.com>
2020-09-11 19:30:49 +0800

    typo: use space

--- src/components/pagination/index.stories.tsx
+++ src/components/pagination/index.stories.tsx
@@ -4,19 +4,19 @@ import { action } from "@storybook/addon-actions";
 import { Pagination } from './index';
 
 export default {
-	title: "Display/Pagination",
-	component: Pagination,
-	decorators: [withKnobs],
+  title: "Display/Pagination",
+  component: Pagination,
+  decorators: [withKnobs],
 };
 
 // export const basicPagination = () => <Pagination defaultCurrent={11}></Pagination>;
 
 export const knobsPagination = () => (
-	<Pagination
-		defaultCurrent={number("defualtCurrent", 1)}
-		total={number("total", 100)}
-		barMaxSize={number("barMaxSize", 5)}
-		pageSize={number("pageSize", 5)}
-		callback={action("callback")}
-	></Pagination>
+  <Pagination
+    defaultCurrent={number("defualtCurrent", 1)}
+    total={number("total", 100)}
+    barMaxSize={number("barMaxSize", 5)}
+    pageSize={number("pageSize", 5)}
+    callback={action("callback")}
+  ></Pagination>
 );
